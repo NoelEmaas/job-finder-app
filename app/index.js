8f909136c3629ee2af61622d1ab963ea2e5efc7f
Noel Emaas <nemaas@gbox.adnu.edu.ph>
2023-06-17 16:28:03 +0800

    feat: add initial files

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text> HOME </Text>
+        </View>
+    );
+};
+
+export default Home;
