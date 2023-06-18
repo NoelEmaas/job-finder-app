13ab907d3e4974ff34d232f3025999bcc9a97608
Noel Emaas <nemaas@gbox.adnu.edu.ph>
2023-06-18 11:49:32 +0800

    feat: add company info section

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -13,7 +13,7 @@ const JobDetails = () => {
     const { data, isLoading, error, reFetch } = useFetch('job-details', { job_id: params.id });
 
     const [refreshing, setRefreshing] = useState(false);
-    
+
     const onRefresh = () => {}
 
     return (
@@ -44,7 +44,23 @@ const JobDetails = () => {
                 <ScrollView showsVerticalScrollIndicator={false} refreshControl={
                     <RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>
                 }>
-
+                    {isLoading ? (
+                        <ActivityIndicator size='large' color={COLORS.primary}/>
+                    ) : error ? (
+                        <Text>Something went wrong</Text>
+                    ) : data.length === 0 ? (
+                        <Text>No Data</Text>
+                    ) : (
+                        <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+                            <Company 
+                            companyLogo={data[0].employer_logo}
+                            jobTitle={data[0].job_title}
+                            companyName={data[0].employer_name}
+                            Location={data[0].job_country}
+                            />
+                            <JobTabs />
+                        </View>
+                    )}
                 </ScrollView>
             </>
         </SafeAreaView>
